910210d09a99e6160bb8c774eeaafe2e55ede545
Sickle41 <sickle41@gmail.com>
2025-06-15 15:10:21 -0500

    Database

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -7,7 +7,7 @@ public class Supplier
     public int Id { get; set; }
     public string Name { get; set; }
     public string ContactInfo { get; set; }
-    public int UserId { get; set; }
+    public int UserProfileId { get; set; }
     public UserProfile userProfile { get; set; }
     public List<MedicationSupplier> MedicationSuppliers { get; set;}
 }
