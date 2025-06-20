6df0b5baf1cc470ef4de1fd08a84d77c51a08376
Sickle41 <sickle41@gmail.com>
2025-06-20 18:28:27 -0500

    PutMedEndpoint

--- client/src/managers/medicationManager.js
+++ client/src/managers/medicationManager.js
@@ -7,3 +7,18 @@ export const getMedications = () => {
 export const expiringSoonMedications = () => {
     return fetch(_apiUrl + "/ExpiringNextMonth").then((res) => res.json());
 }
+
+export const updateMedication = (medication) => {
+    return fetch(`${_apiUrl}/${medication.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(medication),
+    }).then((res) => {
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.status; // Return the status code for success
+    });
+};
